0fc30531e68b6f592d7357e4d74b589acc50d4ce
Shams Benhamou <shamsbenhamou@gmail.com>
2024-08-06 15:36:13 +0200

    wip: dissolve panel

--- src/components/DissolveEffect.jsx
+++ src/components/DissolveEffect.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useRef } from "react";
 import * as kampos from "kampos";
 
+import Project from "./Project";
+
 const DissolveEffect = () => {
   const canvasRef = useRef(null);
 
+  const { Kampos, effects, noise, transitions } = kampos;
+
   useEffect(() => {
-    console.log("DissolveEffect");
-    // Notify when our images are ready
+    const target = document.getElementById("target");
+
     function loadImage(src) {
       return new Promise((resolve) => {
         const img = new Image();
@@ -16,85 +20,192 @@ const DissolveEffect = () => {
         img.src = src;
       });
     }
-    // Get the image URLs
-    const imageFromSrc = document.querySelector("#source-from").src;
-    const imageToSrc = document.querySelector("#source-to").dataset.src;
-    // Load images  and keep their promises so we know when to start
-    const promisedImages = [loadImage(imageFromSrc), loadImage(imageToSrc)];
 
-    const turbulence = kampos.effects.turbulence({
-      noise: kampos.noise.perlinNoise,
+    const mapTarget = document.createElement("canvas");
+
+    // const MAP_HEIGHT = 480;
+    const MAP_HEIGHT = window.innerHeight;
+    const MAP_WIDTH = 500;
+
+    /* Change to true to see the effect with dynamic noise animation */
+    const DYNAMIC = true;
+
+    /* Try flipping between animation types */
+    const TYPE = "LIQUID";
+    //const TYPE = 'SMOKE';
+    const ANIMATIONS = {
+      SMOKE: {
+        octaves: 8,
+        edge: 0.4,
+        cellFactor: 4,
+      },
+      LIQUID: {
+        octaves: 1,
+        edge: 0.03,
+        cellFactor: 2,
+      },
+      CUSTOM: {
+        octaves: 8,
+        edge: 0.3,
+        cellFactor: 4,
+      },
+    };
+
+    mapTarget.width = MAP_WIDTH;
+    mapTarget.height = MAP_HEIGHT;
+
+    /* this factor controls the size of the blobs in the noise - increase for smaller blobs */
+    const AMPLITUDE = ANIMATIONS[TYPE].cellFactor / MAP_WIDTH;
+    const frequency = { x: AMPLITUDE, y: AMPLITUDE };
+
+    /* increase number on range (1, 8) to go from water-like effect into clouds-like one */
+    const octaves = ANIMATIONS[TYPE].octaves;
+
+    /* change to false (or comment out) if you want to see the turbulence noise variant */
+    const isFractal = true;
+
+    /* create the turbulence effect we need for the map texture */
+    const turbulence = effects.turbulence({
+      noise: noise.simplex,
+      frequency,
+      isFractal,
     });
 
-    // Depending of course on the size of the target canvas
-    const WIDTH = 854;
-    const HEIGHT = 480;
-    const CELL_FACTOR = 2;
-    const AMPLITUDE = CELL_FACTOR / WIDTH;
-
-    turbulence.frequency = { x: AMPLITUDE, y: AMPLITUDE };
-    turbulence.octaves = 1;
-    turbulence.isFractal = true;
-
-    const mapTarget = document.createElement("canvas"); // instead of document.createElement('canvas');
-    mapTarget.width = WIDTH;
-    mapTarget.height = HEIGHT;
-
-    const dissolveMap = new kampos.Kampos({
+    const dissolveMap = new Kampos({
       target: mapTarget,
       effects: [turbulence],
       noSource: true,
     });
+
+    /* create the dissolve map by generating a single noise frame */
     dissolveMap.draw();
 
-    const dissolve = kampos.transitions.dissolve();
+    /* you can play with this value on the range of (0.0, 1.0) to go from hard clipping to a smooth smoke-like mask */
+    const high = ANIMATIONS[TYPE].edge;
+
+    /* create the effects/transitions we need */
+    const dissolve = transitions.dissolve({ high });
     dissolve.map = mapTarget;
-    dissolve.high = 0.03; // for liquid-like effect
 
-    const target = document.querySelector("#target");
-    const hippo = new kampos.Kampos({ target, effects: [dissolve] });
+    /* init kampos */
+    const instance = new Kampos({ target, effects: [dissolve] });
 
-    Promise.all(promisedImages)
-      .then(([fromImage, toImage]) => {
-        hippo.setSource({ media: fromImage, width: WIDTH, height: HEIGHT });
+    /* make sure videos are loaded and playing*/
+    Promise.all([
+      loadImage(`img/blank.png`),
+      loadImage(`img/fullblack.png`),
+    ]).then((images) => {
+      const width = MAP_WIDTH;
+      const height = MAP_HEIGHT;
+      let index = 0;
 
-        dissolve.to = toImage;
-      })
-      .then(function () {
-        hippo.play((time) => {
-          dissolve.progress = Math.abs(Math.sin(time * 4e-4)); // multiply time by a factor to slow it down a bit
+      if (DYNAMIC) {
+        dissolve.textures[1].update = true;
+      }
+
+      /* paint initial scene */
+      instance.setSource({ media: images[0], width, height });
+      dissolve.to = images[1];
+      instance.draw();
+
+      function easeOutCubic(x) {
+        return 1 - Math.pow(1 - x, 3);
+      }
+
+      function changeImage(prevImage, nextImage) {
+        /* set media source */
+        instance.setSource({ media: prevImage, width, height });
+
+        /* set media to transition into */
+        dissolve.to = nextImage;
+
+        const start = performance.now();
+
+        /* start kampos */
+        instance.play(function draw() {
+          const time = performance.now() - start;
+
+          /* this is invoked once in every animation frame */
+          if (DYNAMIC) {
+            turbulence.time = time * 2;
+            dissolveMap.draw();
+          }
+
+          /* you can reduce time factor for slower transition, or increase for faster */
+          const progress = easeOutCubic(time * (DYNAMIC ? 2e-4 : 4e-4) * 2.5);
+          dissolve.progress = progress;
+
+          if (progress * 100 >= 99.9) {
+            instance.stop();
+
+            // bind the event again
+            bindClick();
+          }
         });
-      });
+      }
+
+      function next() {
+        const from = images[index];
+
+        // next...
+        index = (index + 1) % images.length;
+
+        const to = images[index];
+
+        changeImage(from, to);
+      }
+
+      function prev() {
+        const from = images[index];
+
+        // prev...
+        index = (index - 1 + images.length) % images.length;
+
+        const to = images[index];
+
+        changeImage(from, to);
+      }
+
+      function startAnim(event) {
+        next();
+      }
+
+      // function bindClick() {
+      //   target.classList.add("clickable");
+      //   target.addEventListener("click", click, { once: true });
+      // }
+
+      // bindClick();
+      setTimeout(() => {
+        next();
+      }, 1000);
+    });
 
     return () => {
       dissolveMap.destroy();
-      hippo.destroy();
+      instance.destroy();
     };
   }, []);
 
   return (
-    <div
-      style={{
-        width: "854px",
-        height: "480px",
-        overflow: "hidden",
-        position: "absolute",
-        top: "0",
-        left: "0",
-        zIndex: "1000",
-        outline: "red solid 1px",
-      }}
-      className="diss"
-    >
-      <canvas ref={canvasRef} id="target">
-        <img
-          id="source-from"
-          src="img/thumbs/boinaud.png"
-          alt="My first image"
-        />
-        <img id="source-to" data-src="img/tree.jpg" alt="My second image" />
-      </canvas>
-    </div>
+    <>
+      <div
+        style={{
+          width: "854px",
+          height: "480px",
+          overflow: "hidden",
+          position: "absolute",
+          top: "0",
+          left: "0",
+          zIndex: "1",
+          // outline: "red solid 1px",
+        }}
+        className="diss"
+      >
+        <canvas ref={canvasRef} id="target"></canvas>
+      </div>
+      <Project />
+    </>
   );
 };
 
---
+++ src/components/Project.jsx
@@ -0,0 +1,29 @@
+import { useRef } from "react";
+
+export default function Project() {
+  const projectRef = useRef(null);
+
+  return (
+    <>
+      <div className="project">
+        <div className="project-content">
+          <h1>Hello</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+            doloremque quasi est fuga debitis accusamus, ut qui deleniti,
+            incidunt neque placeat repudiandae autem nostrum suscipit ratione ab
+            expedita nulla libero dicta doloribus nihil accusantium. Quidem
+            harum deleniti sequi tempore quas veritatis architecto, dolor dolore
+            quasi unde, vel ipsam numquam ipsa voluptates quos nemo
+            exercitationem modi, debitis laborum iste ullam quis explicabo
+            doloremque. Recusandae, assumenda atque ipsum vel laborum autem ad
+            impedit dolor sed hic sapiente repellat iusto. Illum repudiandae
+            dignissimos, ducimus consectetur, dolores tempora dicta iure quod
+            cupiditate esse perferendis facilis laborum modi? Ex sunt totam qui?
+            Veniam, culpa eum.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
